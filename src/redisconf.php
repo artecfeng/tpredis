9f476aaaa2184689dc0d52de22d8daca9b4dca18
冯腾举 <824431999@qq.com>
2018-09-05 15:38:24 +0800

    thinkphp5 redis

---
+++ src/redisconf.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: tengjufeng
+     * Date: 2018/9/5
+     * Time: 下午3:08
+     */
+
+    return [
+        'host' => '127.0.0.1',
+
+        'port' => 6379,
+
+        'password' => '',
+
+        'select' => 0,
+
+        'timeout' => 0,
+
+        'expire'     => 0,
+        //是否是长连接
+        'persistent' => false,
+        //前缀
+        'prefix'     => '',
+    ];
